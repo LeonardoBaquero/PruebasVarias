7c9576827d9688ee04c8bb8a016d7311796b0f88
LeonardoBaquero <42071926+LeonardoBaquero@users.noreply.github.com>
2025-12-03 17:46:56 -0500

    Tarea de SQL

---
+++ 41-tarea-aggregation.sql
@@ -0,0 +1,96 @@
+-- 1. Cuantos usuarios tenemos con cuentas @google.com
+-- Tip: count, like
+    select count(*) as cantidad,
+           substring(email, position('@' in email)+1) as Dominio
+               from users
+               where email like '%@google.com'
+    group by
+        substring(email, position('@' in email)+1);
+
+-- 2. De qué países son los usuarios con cuentas de @google.com
+-- Tip: distinct
+
+select distinct country,
+           substring(email, position('@' in email)+1) as Dominio
+               from users
+               where email like '%@google.com';
+
+-- 3. Cuantos usuarios hay por país (country)
+-- Tip: Group by
+select country, count(*) as Cantidad from users
+group by country;
+
+-- 4. Listado de direcciones IP de todos los usuarios de Iceland
+-- Campos requeridos first_name, last_name, country, last_connection
+select first_name,
+       last_name,
+       country,
+       last_connection
+       from users
+where country = 'Iceland';
+
+-- 5. Cuantos de esos usuarios (query anterior) tiene dirección IP
+-- que incia en 112.XXX.XXX.XXX
+
+select first_name,
+       last_name,
+       country,
+       last_connection
+       from users
+where country = 'Iceland' and last_connection like '112.%';
+
+
+-- 6. Listado de usuarios de Iceland, tienen dirección IP
+-- que inicia en 112 ó 28 ó 188
+-- Tip: Agrupar condiciones entre paréntesis
+
+select first_name,
+       last_name,
+       country,
+       last_connection
+       from users
+where country = ('Iceland') and ((last_connection like '112.%') or (users.last_connection like '28.%') or (users.last_connection like '188.%'));
+
+-- 7. Ordene el resultado anterior, por apellido (last_name) ascendente
+-- y luego el first_name ascendentemente también
+
+    select first_name,
+       last_name,
+       country,
+       last_connection
+       from users
+where country = ('Iceland') and ((last_connection like '112.%') or (users.last_connection like '28.%') or (users.last_connection like '188.%'))
+    order by last_name;
+
+    select first_name,
+       last_name,
+       country,
+       last_connection
+       from users
+where country = ('Iceland') and ((last_connection like '112.%') or (users.last_connection like '28.%') or (users.last_connection like '188.%'))
+    order by first_name desc;
+
+
+-- 8. Listado de personas cuyo país está en este listado
+-- ('Mexico', 'Honduras', 'Costa Rica')
+-- Ordenar los resultados de por País asc, Primer nombre asc, apellido asc
+-- Tip: Investigar IN
+-- Tip2: Ver Operadores de Comparación en la hoja de atajos (primera página)
+
+select first_name,
+       last_name,
+       country
+    from users
+where country in ('Mexico', 'Honduras', 'Costa Rica')
+order by  country asc;
+
+-- 9. Del query anterior, cuente cuántas personas hay por país
+-- Ordene los resultados por País asc
+
+select
+       country,
+       count(*) AS cantidad_por_Pais
+    from users
+where country in ('Mexico', 'Honduras', 'Costa Rica')
+group by country
+order by  country asc;
